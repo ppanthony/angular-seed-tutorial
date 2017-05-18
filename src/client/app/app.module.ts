e9a255a406ef4a6c957e4c000466c6ed11efe0e2
Paul Anthony <paul@getdeckchair.com>
2017-05-18 17:55:20 +0100

    first commit

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -4,15 +4,17 @@ import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import { HeroDetailComponent } from './hero-detail.component';
 
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
-  declarations: [AppComponent],
+  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot(), FormsModule],
+  declarations: [AppComponent,HeroDetailComponent],
   providers: [{
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
