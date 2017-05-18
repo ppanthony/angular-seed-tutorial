c82fe309e78c6f86545664470e6f5cb060718b45
Paul Anthony <paul@getdeckchair.com>
2017-05-18 23:23:26 +0100

    ready to add routing

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -5,17 +5,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-
+import { AppComponent }        from './app.component';
+import { HeroesComponent }     from './heroes.component';
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot(), FormsModule],
-  declarations: [AppComponent,HeroDetailComponent],
-  providers: [{
+  declarations: [AppComponent,HeroDetailComponent, HeroesComponent],
+  providers: [ HeroService, {
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
   }],
