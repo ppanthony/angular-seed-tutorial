4f645f130266580eae51319863b1cbb237fb3e17
Paul Anthony <paul.anthony@propertypal.com>
2017-05-19 09:32:20 +0100

    removed some of the seed controllers. added routing

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template : `
+
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
   `
 })
 
--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,26 +1,35 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';;
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes.component';
-import { AboutModule } from './about/about.module';
-import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot(), FormsModule],
+  imports: [BrowserModule, HttpModule, AppRoutingModule, SharedModule.forRoot(), FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
   declarations: [AppComponent,HeroDetailComponent, HeroesComponent],
   providers: [ HeroService, {
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
   }],
   bootstrap: [AppComponent]
-
 })
-export class AppModule { }
+
+
+
+export class AppModule {
+
+}
